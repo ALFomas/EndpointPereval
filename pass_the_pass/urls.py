a33ea3db8e2175e075b71b04a0dc5d022b1415d9
ALFomas <13fominal@gmail.com>
2024-07-11 19:03:05 +0300

    configuring urls

--- pass_the_pass/urls.py
+++ pass_the_pass/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SubmitData
+from .views import SubmitData, CampingDetailView
 
 urlpatterns = [
     path('submitData/', SubmitData.as_view(), name='submit_data'),
+    path('detailData/<int:id>/', CampingDetailView.as_view(), name='camping_detail'),
+
 ]
