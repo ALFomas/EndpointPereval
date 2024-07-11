3744c38c42819c8b9c58dd6d8b1b80869f4a7c54
ALFomas <13fominal@gmail.com>
2024-07-11 14:58:06 +0300

    create serializers.py. add 'rest_framework' in settings.py

--- Pereval/settings.py
+++ Pereval/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pass_the_pass'
+    'pass_the_pass',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
